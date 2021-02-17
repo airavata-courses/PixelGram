e89795dcf7ef6b39f0cfd24bdcfc5bf030e492e4
Suraj Gupta Gudla <surajgupta268@gmail.com>
2021-02-17 17:50:03 -0500

    basic login page UI

---
+++ loginPageUI/src/components/login.component.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+export default class Login extends Component {
+    render() {
+        return (
+            <form>
+                <h3>Sign in</h3>
+                <div className="form-group">
+                    <label>Email</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+
+                <div className="form-group">
+                    <div className="custom-control custom-checkbox">
+                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                    </div>
+                </div>
+
+                <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
+                <p className="forgot-password text-right">
+                     <a href=" ">Forgot password?</a>
+                </p>
+
+                <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in with IU</button>
+                <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in with Google</button>
+                <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in with GitHub</button>
+            </form>
+        );
+    }
+}
