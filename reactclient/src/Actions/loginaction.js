005d141de73970aa92a8ae43ac9d84ef58fa58b5
sravya160597 <garagas16@gmail.com>
2021-02-21 20:48:52 -0500

    Add files via upload

---
+++ reactclient/src/Actions/loginaction.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import login from ".Actions/login.component";
+const { creds, accountFlag } = require('./account_pb.js');
+const {}
+class index extends Component {
+    callGrpcService = () => {
+        const request = new creds();
+        request.setEmail(email);
+        request.setPassword(password)
+
+        client.loginUser(request, {}, (err, response) => {
+            if (response == null) {
+              console.log(err)
+            }else {
+              console.log(response.getIssuccess())
+            }
+        });
+}
+}    
