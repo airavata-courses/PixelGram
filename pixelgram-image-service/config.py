cae0c2880ddfd33cf70623b8494df968a6b26c37
Krishna Sannidhi <sannidhikrishna212@gmail.com>
2021-04-07 02:53:48 -0400

    Only User Service

--- pixelgram-image-service/config.py
+++
@@ -1,4 +0,0 @@
-RABBITMQ_HOST = 'rabbitmq-service'
-RABBITMQ_PORT = 5672
-USER_TO_IMAGE_QUEUE = 'usertoimage_queue'
-IMAGE_DETAILS_QUEUE = 'imagedetails_queue'
--- pixelgram-ui/src/store/actionTypes.js
+++
@@ -1,3 +0,0 @@
-export const USER_LOGOUT = "USER_LOGOUT";
-export const USER_LOGIN = "USER_LOGIN";
-
