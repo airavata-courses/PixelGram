50ee27f3daf2c9be50ff22a23522ed2539c5b686
Krishna Sannidhi <sannidhikrishna212@gmail.com>
2021-04-08 03:46:13 -0400

    user service test cases

--- pixelgram-user-service/app/userfunctions_test.py
+++ pixelgram-user-service/app/userfunctions_test.py
@@ -3,46 +3,53 @@ from models.user import authentication
 from models.user import search_by_username
 from models.user import UserModel
 
-username="sra"
-password="123456"
-userid = "d8534c92-56a9-4c3d-a969-876badd284e2"
-t = []
-a = UserModel
-newpassword = "123456"
 
+t = []
+a = UserModel("abcd1234","bharath","000000")
+username="bharath"
+password="000000"
+userid = "abcd1234"
+newpassword = "000000"
+
+def test_createuser():
+     a.create_new_user()
+    
 def test_authentication():
     temp=authentication(username,password)
     assert temp==({'message': 'User verified'},200),"authentication failed, check creds"
 
-def test_searchbyusername():
-    t=search_by_username("s")
-    print(t)
+# def test_searchbyusername():
+#     t=search_by_username("s")
+#     print(t)
 
 def test_findbyusername():
     b = a.find_by_username(username)
+    print(b.username)
+    print(b.password)
+    print(b.userid)
     assert (b.username == username),"username doesnot match"
     assert(b.password == password),"password doesnot exist"
     assert(b.userid == userid),"no userid"
-    print(b.username)
-    print(b.password)
 
 def test_findbyuserid():
     c = a.find_by_userid(userid)
+    print(c.username)
     assert (c.username == username),"username doesnot match"
     assert(c.password == password),"password doesnot exist"
     assert(c.userid == userid),"no userid"
 
 def test_updatepassword():
-    a.username = "sra"
-    a.password = "123456"
-    a.userid = "d8534c92-56a9-4c3d-a969-876badd284e2"
-    a.update_password(a,newpassword)
+    # a.username = "bharath"
+    # a.password = "123456"
+    # a.userid = "abcd1234"
+    a.update_password(newpassword)
     temp=authentication(username,newpassword)
     assert temp==({'message': 'User verified'},200),"authentication failed, check creds"
 
 if __name__ == "__main__":
+    test_createuser()
     test_authentication()
-    test_searchbyusername()
+    # test_searchbyusername()
     test_findbyusername()
     test_findbyuserid()
     test_updatepassword()
