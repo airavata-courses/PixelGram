b032dd764c7e97e7b128dfb4ac759a6e966cb907
Krishna Sannidhi <sannidhikrishna212@gmail.com>
2021-04-18 10:36:58 -0400

    redirecting requests to gateway

--- pixelgram-ui/app/src/helperClasses/axiosService.js
+++ pixelgram-ui/app/src/helperClasses/axiosService.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 //import { getCookie_Token } from './LocalStorage';
 
-// axios.defaults.baseURL = process.env.REACT_APP_SERVICE_PATH;
+axios.defaults.baseURL = process.env.REACT_GATEWAY_SERVICE;
 
-// console.log('base url', process.env.REACT_APP_SERVICE_PATH)
+console.log('base url', process.env.REACT_GATEWAY_SERVICE)
 
 // axios.interceptors.request.use(config => {
 //     console.log("request ", config);
